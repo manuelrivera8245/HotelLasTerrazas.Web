53b2f538c56d4153fd67488306c55370e3b4b2f1
Manuel Rivera <manuelrivera8245@gmail.com>
2026-09-21 18:51:09 -0500

    Añade directivas using y limpia comentarios en DAOs
    Se agregan directivas using para acceso a SQL Server y modelos en Conexion.cs, HabitacionDAO.cs, PagoDAO.cs y ReservaDAO.cs. Se elimina un comentario XML redundante en Conexion.cs.

--- HotelLasTerrazas.Web/DataAccess/Conexion.cs
+++ HotelLasTerrazas.Web/DataAccess/Conexion.cs
@@ -2,13 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Configuration;
+using System.Data.SqlClient;
 
 namespace HotelLasTerrazas.Web.DataAccess
 {
-    /// <summary>
     /// Provee una conexión SqlConnection lista para usar,
     /// leyendo la cadena de conexión desde Web.config.
-    /// </summary>
     public static class ConexionBD
     {
         private static readonly string cadenaConexion =
