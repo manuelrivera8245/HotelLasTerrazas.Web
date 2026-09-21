9ce1fb36a6e6773b80dae67f946a94a26af83dbd
Jeremy Reyes <jerreyesga@uch.pe>
2026-09-21 18:00:37 -0500

    Añade PagoDAO para registrar y listar métodos de pago
    Se agregó el archivo PagoDAO.cs al proyecto HotelLasTerrazas.Web.csproj. PagoDAO implementa métodos para registrar pagos usando el procedimiento almacenado sp_RegistrarPago y para listar métodos de pago activos desde la base de datos. Se actualizó el archivo .csproj para incluir la nueva clase.

---
+++ HotelLasTerrazas.Web/DataAccess/PagoDAO.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HotelLasTerrazas.Web.DataAccess
+{
+    public class PagoDAO
+    {
+        /// <summary>
+        /// Llama a sp_RegistrarPago. Registra el pago y genera el comprobante en una sola operación.
+        /// </summary>
+        public int Registrar(Pago pago)
+        {
+            using (var conexion = ConexionBD.ObtenerConexion())
+            using (var comando = new SqlCommand("sp_RegistrarPago", conexion))
+            {
+                comando.CommandType = CommandType.StoredProcedure;
+                comando.Parameters.AddWithValue("@IdReserva", pago.IdReserva);
+                comando.Parameters.AddWithValue("@IdMetodoPago", pago.IdMetodoPago);
+                comando.Parameters.AddWithValue("@IdTipoComprobante", pago.IdTipoComprobante);
+                comando.Parameters.AddWithValue("@Serie", pago.Serie);
+                comando.Parameters.AddWithValue("@Numero", pago.Numero);
+
+                conexion.Open();
+                object resultado = comando.ExecuteScalar();
+                return Convert.ToInt32(resultado);
+            }
+        }
+
+        public List<MetodoPago> ListarMetodosPago()
+        {
+            var lista = new List<MetodoPago>();
+
+            using (var conexion = ConexionBD.ObtenerConexion())
+            using (var comando = new SqlCommand("SELECT * FROM MetodoPago WHERE Estado = 1", conexion))
+            {
+                conexion.Open();
+                using (var lector = comando.ExecuteReader())
+                {
+                    while (lector.Read())
+                    {
+                        lista.Add(new MetodoPago
+                        {
+                            IdMetodoPago = Convert.ToInt32(lector["IdMetodoPago"]),
+                            NombreMetodo = lector["NombreMetodo"].ToString()
+                        });
+                    }
+                }
+            }
+            return lista;
+        }
+    }
+}
