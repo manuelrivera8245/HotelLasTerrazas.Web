67b084da91d85660230079127f22a1e69c5dbde0
Jeremy Reyes <jerreyesga@uch.pe>
2026-09-21 18:26:49 -0500

    Añade clase ConexionBD para gestión de conexiones SQL
    Se incorpora el archivo Conexion.cs con la clase estática ConexionBD en el espacio de nombres HotelLasTerrazas.Web.DataAccess. Esta clase centraliza la obtención de conexiones SqlConnection usando la cadena "HotelConnection" de Web.config, facilitando el acceso a la base de datos en el proyecto.

---
+++ HotelLasTerrazas.Web/DataAccess/Conexion.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HotelLasTerrazas.Web.DataAccess
+{
+    /// <summary>
+    /// Provee una conexión SqlConnection lista para usar,
+    /// leyendo la cadena de conexión desde Web.config.
+    /// </summary>
+    public static class ConexionBD
+    {
+        private static readonly string cadenaConexion =
+            ConfigurationManager.ConnectionStrings["HotelConnection"].ConnectionString;
+
+        public static SqlConnection ObtenerConexion()
+        {
+            return new SqlConnection(cadenaConexion);
+        }
+    }
+}
